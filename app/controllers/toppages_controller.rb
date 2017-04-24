7af455f391ae5e557184f110d5c452069a7f0727
jiro.kirameki <shimojima1012@gmail.com>
2017-04-24 07:36:12 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @user = current_user
+      @tasklist = current_user.microposts.build  # form_for 用
+      @tasklists = current_user.microposts.order('created_at DESC').page(params[:page])
+    end
   end
 end
